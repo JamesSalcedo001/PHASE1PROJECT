ed7583c07a7ae9e185e257b5921cf30a791bf5f5
JamesSalcedo <jamezzzsalcedo17@gmail.com>
2022-10-08 22:00:06 -0500

    added more css effects to dark mode button with a hover effect moving the button when it's hovered over. added css to title, J.S. POKEDEX, similar hover effect but different color. added some more css to cards to make them move when hovered over, and changed the images to a different type, as well as added another attribute (pokemon type). managed to get the javascript keydown event handler to loop through another set of 25 items, as well as remove the previous ones from the page, however it still has some issues where the cards don't display evenly and it won't retrieve an additional 25 pokemon it just keeps it at the second set.

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 const pokeBox = document.getElementById("pokeBox")
 
-// document.addEventListener("DOMContentLoaded", pokeLoop);
-
+document.addEventListener("DOMContentLoaded", pokeLoop)
 
 
 function pokeLoop() {
   let pokeNum = 25;
   for(let i = 1; i < pokeNum; i++) {
+    
     catchPokemon(i)
     document.addEventListener("keydown", (e) => {
       if (e.defaultPrevented) {
@@ -14,7 +14,11 @@ function pokeLoop() {
       }
       switch (e.key) {
         case "Enter": catchPokemon(i=i+25);
-        replaceChildren();
+        pokeBox.replaceChildren();
+        let pokeNum = 50
+        for(let i = 26; i < pokeNum; i++) {
+          catchPokemon(i)
+        }
         break; 
         default:
           return;
@@ -23,8 +27,8 @@ function pokeLoop() {
     }, true);
   }
 }
+// pokeLoop();
 
-pokeLoop()
 
 function catchPokemon(id) {
   fetch(`https://pokeapi.co/api/v2/pokemon/${id}`)
@@ -39,15 +43,18 @@ function renderPokemon(pokemon) {
   const pCard = document.createElement("div")
   const name = document.createElement("h2")
   const img = document.createElement("img")
+  const type1 = document.createElement("h5")
 
   pCard.className = "pCard"
-
+  type1.innerText = pokemon.types["0"].type.name
+  type1.className = "type1"
+  type1.textContent = pokemon.types["0"].type.name.toUpperCase();
   name.textContent = pokemon.name.toUpperCase();
 
-  img.src = pokemon.sprites.front_default
+  img.src = pokemon.sprites.versions["generation-vii"]["ultra-sun-ultra-moon"]["front_default"];
   img.className = "pokeImg"
 
-  pCard.append(name, img)
+  pCard.append(name, img, type1)
   pokeBox.append(pCard)
 
 }
@@ -69,15 +76,38 @@ let darkButton = document.getElementById("darkMode")
 darkButton.addEventListener("click", darkMode)
 
 
-// document.addEventListener("keydown", (e) => {
-//   if (e.defaultPrevented) {
-//     return;
+
+
+
+"added more css effects to dark mode button with a hover effect moving the button when it's hovered over. added css to title, J.S. POKEDEX, similar hover effect but different color. added some more css to cards to make them move when hovered over, and changed the images to a different type, as well as added another attribute (pokemon type). managed to get the javascript keydown event handler to loop through another set of 25 items, as well as remove the previous ones from the page, however it still has some issues where the cards don't display evenly and it won't retrieve an additional 25 pokemon it just keeps it at the second set. "
+
+
+
+
+
+// THIS WORKS TO ADD NEW POKEMON TO THE PAGE HOWEVER IT IS BUGGY
+
+// function pokeLoop() {
+//   let pokeNum = 25;
+//   for(let i = 1; i < pokeNum; i++) {
+    
+//     catchPokemon(i)
+//     document.addEventListener("keydown", (e) => {
+//       if (e.defaultPrevented) {
+//         return;
+//       }
+//       switch (e.key) {
+//         case "Enter": catchPokemon(i=i+25);
+//         pokeBox.replaceChildren();
+//         let pokeNum = 50
+//         for(let i = 26; i < pokeNum; i++) {
+//           catchPokemon(i)
+//         }
+//         break; 
+//         default:
+//           return;
+//       }
+//       e.preventDefault();
+//     }, true);
 //   }
-//   switch (e.key) {
-//     case "Enter": ;
-//     break; 
-//     default:
-//       return;
-//   }
-//   e.preventDefault();
-// }, true);
+// }
